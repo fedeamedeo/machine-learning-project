f07decee8282dd5aadf8330f690eb9d97c4289d7
fedeamedeo <federica.amedeopriv@gmail.com>
2025-05-13 15:53:13 +0200

    .

--- app3.py
+++ app3.py
@@ -26,7 +26,7 @@ recs_df, items_df, interactions_df = load_data()
 # ---------- SIDEBAR ----------
 st.sidebar.title("🔧 Settings")
 st.sidebar.markdown("Chat with the system to get personalized book recommendations using precomputed TF-IDF matches.")
-st.session_state.selected_user = st.sidebar.selectbox("Select a User ID", recs_df['user_id'].unique(), index=0)
+st.session_state.selected_user = st.sidebar.selectbox("Select your Libray User number", recs_df['user_id'].unique(), index=0)
 
 
 
